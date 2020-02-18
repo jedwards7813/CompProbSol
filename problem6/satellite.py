0b0add883687d41fca862b5b038237631daa56c8
jedwards7813 <59661851+jedwards7813@users.noreply.github.com>
2020-02-18 12:41:30 -0500

    Update satellite.py

--- problem6/satellite.py
+++ problem6/satellite.py
@@ -21,19 +21,19 @@ elif un == "min" or un == "m":
 else:
     print("bad input")
     
-G = 6.67*10^(-11);
-M = 5.98*10^(24);
-R = 6.371*10^(6);
+G = 6.67*10**(-11);
+M = 5.98*10**(24);
+R = 6.371*10**(6);
 
 
-h = (G*M*t^2/(4*np.pi^2))^(1/3) - R;
+h = (G*M*t**2/(4*np.pi**2))**(1/3) - R;
 v = np.sqrt(G*M/(h+R))
 vkm=v/1000
 hkm=h/1000
 print('Orbits at a altitude of {0:.1f} km and at a speed of {1:.1f} km/s.'.format(hkm,vkm))
 T = np.arange(1.5,60)
 t = 3600*T
-h = (G*M*t^2/(4*np.pi^2))^(1/3) - R;
+h = (G*M*t**2/(4*np.pi**2))**(1/3) - R;
 v = np.sqrt(G*M/(h+R))
 hkm = h/1000
 vkm = v/1000
