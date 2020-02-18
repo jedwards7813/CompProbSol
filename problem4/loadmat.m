16a89093b0f02cdcf0ae9285129fdfa642423450
jedwards7813 <59661851+jedwards7813@users.noreply.github.com>
2020-02-18 12:41:28 -0500

    Update loadmat.m

--- problem4/loadmat.m
+++ problem4/loadmat.m
@@ -1,2 +1,6 @@
 % Starting point for Problem 4
 load('smallperiodictable')
+readtable('smallperiodictable')
+d1=find(density>1);%atomic numbers of elements w/ density>1
+d2=names(x)%names of elements w/ density>1
+num=numel(x)%# of elements w/ density>1
